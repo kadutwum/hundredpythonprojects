40a2bf7bab3ab757af4bb9da061dab8e12c9f299
Kofi Adutwum <adko78@gmail.com>
2021-03-16 23:02:01 -0400

    recode project1 using string.format() and comments

--- project1.py
+++ project1.py
@@ -1,3 +1,5 @@
+
+# Obtain User inputs 
 item_1 = input("what is the first item in your basket? ")
 price_1 = float(input("what is the price of this? "))
 item_2 = input("what is the second item in your basket? ")
@@ -5,19 +7,24 @@ price_2 = float(input("what is the price of this? " ))
 item_3 = input("what is the third item in your basket? ")
 price_3 = float(input("what is the price of this? "))
 total = price_1 + price_2 + price_3
-message ="Thank for shopping with us."
 
+# declare the static components of the receipt
+message ="Thank for shopping with us."
+address_name = "Python Academy, Inc."
+address_street = "1502 Somewhere Ave."
+address_city = "CityName, State"
+
+# print out receipt
 print("*" * 50)
-print("Python Academy, Inc. ")
-print("1502 Somewhere Ave.\nCityName, State  ")
+print("\t\t{}\n\t\t{}\n\t\t{}".format(address_name, address_street, address_city))#using string.format() to output company name and address
 print("=" * 50)
-print("Product Name \t\t Product Type")
-print(item_1 + "\t\t\t $"+ str(price_1))
-print(item_2 + "\t\t\t $"+ str(price_2))
-print(item_3 + "\t\t\t $"+ str(price_3))
+print("\tProduct Name\t\tProduct Type")
+print("\t{}\t\t\t{}".format(item_1, price_1)) #print(item_1 + "\t\t\t $"+ str(price_1)) will produce the same output
+print("\t{}\t\t\t{}".format(item_2, price_2))
+print("\t{}\t\t\t{}".format(item_3, price_3))
 print("=" * 50)
-print("\t\t\t Total")
-print("\t\t\t $" +  str(total))
+print("\t\t\t\tTotal")
+print("\t\t\t\t${}".format(total))
 print("=" * 50)
-print(message )
+print("\t\t{}".format(message))
 print("*" * 50)
